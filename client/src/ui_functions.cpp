e202426462a1381111265bc5146ba4cea6e0b4d7
Ilya <iluxa.kholkin@gmail.com>
2022-02-11 00:47:19 +0300

    ui_functions: add a usefull function

--- client/src/ui_functions.cpp
+++ client/src/ui_functions.cpp
@@ -1,20 +1,27 @@
 #include "../include/ui_functions.h"
 #include <vector>
 #include <string>
-#include <SFML/Graphics.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
-#include <TGUI/TGUI.hpp>
 
 namespace war_of_ages {
 
 void place_widgets(std::vector<tgui::Widget::Ptr> &widgets, int width, int height, int space) {
-    int number = static_cast<int>(widgets.size());
-    int length = number * (height + space) - space;
+
+    /// Places widgets in the center of a screen in the order specified by their order in widgets vector.
+    ///
+    /// Width: width of each Widget in percent of screen width. (default = 40)
+    /// Height: height of each Widget in percent of screen height. (default = 10)
+    /// Space: a gap between each pair of adjacent Widgets in percent of screen height. (default = 5)
+    ///
+    /// Normally you don't want to specify width, height or space parameters, because it will lead to
+    /// differences between screens.
+
+    int length = static_cast<int>(widgets.size()) * (height + space) - space;
     int cur_y_pos = (100 - length) / 2;
     int x_pos = (100 - width) / 2;
     for (auto &w: widgets) {
-        w->setSize(tgui::String(std::to_string(width) + "%"), tgui::String(std::to_string(height) + "%"));
-        w->setPosition(tgui::String(std::to_string(x_pos) + "%"), tgui::String(std::to_string(cur_y_pos) + "%"));
+        w->setSize(get_layout(width, height));
+        w->setPosition(get_layout(x_pos, cur_y_pos));
         cur_y_pos += height + space;
     }
 }
@@ -22,6 +29,10 @@ void place_widgets(std::vector<tgui::Widget::Ptr> &widgets, int width, int heigh
 void show_screen(tgui::Gui &gui, const tgui::String &new_screen_group, const tgui::String &prev_screen_group) {
     gui.get(prev_screen_group)->setVisible(false);
     gui.get(new_screen_group)->setVisible(true);
+}
+
+tgui::Layout2d get_layout(int width_percent, int height_percent) {
+    return {tgui::String(std::to_string(width_percent) + "%"), tgui::String(std::to_string(height_percent) + "%")};
 }
 
 }
