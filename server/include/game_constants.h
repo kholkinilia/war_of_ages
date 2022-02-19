bc57dadf5e7f880a527f70f36af324f5e055b2a1
Vakhtang <evm35rus@gmail.com>
2022-02-19 22:11:22 +0300

    updated game_constants.h

--- server/include/game_constants.h
+++ server/include/game_constants.h
@@ -1,11 +1,15 @@
 #ifndef WAR_OF_AGES_GAME_CONSTANTS_H
 #define WAR_OF_AGES_GAME_CONSTANTS_H
 
+namespace war_of_ages {
+
 const int NUM_OF_AGES = 4;
 const int UNITS_PER_AGE = 3;
+const int CANNONS_PER_AGE = 3;
 const int MAX_CANNON_SLOTS = 3;
 const int UNITS_QUEUE_SIZE = 5;
 
 const int INITIAL_MONEY = 200;
+}  // namespace war_of_ages
 
 #endif  // WAR_OF_AGES_GAME_CONSTANTS_H
