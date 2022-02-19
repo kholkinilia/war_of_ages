3e38e11c2579c5a7853b22c3ae8fb68b460ab04d
Vakhtang <evm35rus@gmail.com>
2022-02-19 20:36:42 +0300

    updated bullet and player classes

--- server/include/player.h
+++ server/include/player.h
@@ -2,9 +2,11 @@
 #define WAR_OF_AGES_PLAYER_H
 
 #include <deque>
+#include <mutex>
 #include <vector>
 #include "bullet.h"
 #include "cannon.h"
+#include "game_constants.h"
 #include "unit.h"
 
 namespace war_of_ages {
@@ -12,12 +14,13 @@ struct player {
     player() noexcept;
 
     void update(std::vector<unit> &enemies, int time);  // need all enemies for ult
-    void buy_unit(int unit_level);     // unit_level in [0, NUM_OF_UNITS=3), unit_type = NUM_OF_AGES *
+    void buy_unit(int unit_level);     // unit_level in [0, NUM_OF_UNITS_IN_AGE), unit_type = NUM_OF_AGES *
                                        // m_age + unit_level ???
     void buy_cannon(int cannon_type);  // the same remark
     void buy_cannon_slot();
     void sell_cannon(int slot);
     void use_ult();
+    void clear_dead_objects();
 
     // Getters
 
@@ -33,13 +36,14 @@ struct player {
 private:
     int m_age = 0;  // TODO: replace with enum
     int m_exp = 0;
-    int m_money;  // TODO: = INITIAL_MONEY = ?
+    int m_money = INITIAL_MONEY;
     int m_last_ult = 0;
     std::deque<unit> m_units = {};
     std::vector<bullet> m_bullets = {};
     std::vector<cannon> m_cannons = {};
+    std::deque<int> m_units_to_train;
 
-    std::deque<int> m_units_to_train;  // TODO: should think about limit of queue's size (5?)
+    mutable std::mutex m_mutex;
 };
 }  // namespace war_of_ages
 
--- server/src/bullet.cpp
+++ server/src/bullet.cpp
@@ -2,13 +2,17 @@
 #include "cmath"
 
 namespace war_of_ages {
-/*bullet::bullet(int type, int x_start, int y_start, int x_target, int y_target) noexcept
-    : m_type(type), m_x(x_start), m_y(y_start) {
-    // TODO: think how to normalize m_vx and m_vy
-}*/
+
+// FIXME: think how to normalize m_vx and m_vy
+bullet::bullet(int type, int x_start, int y_start, int x_target, int y_target) noexcept
+    : m_type(type), m_x(x_start), m_y(y_start), m_vx{}, m_vy{} {
+}
+
+void bullet::update(std::deque<unit> &enemies, int dt) {
+    // TODO: implement
+}
 
 // Getters
-// TODO: use mutexes
 
 [[nodiscard]] int bullet::type() const {
     return m_type;
@@ -22,8 +26,21 @@ namespace war_of_ages {
     return m_y;
 }
 
+[[nodiscard]] int bullet::vx() const {
+    return m_vx;
+}
+
+[[nodiscard]] int bullet::vy() const {
+    return m_vy;
+}
+
 [[nodiscard]] bool bullet::is_alive() const {
     return m_is_alive;
+}
+
+[[nodiscard]] bullet_stats bullet::get_stats(int bullet_type) {
+    return {};
+    // TODO: return stats
 }
 
 }  // namespace war_of_ages
