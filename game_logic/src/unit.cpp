2748ad12aa59c15b7652d8f93a226d66f09e6c7c
Ilya <iluxa.kholkin@gmail.com>
2022-03-13 17:29:44 +0300

    Add vec2f(int, int). Replace all coords inf unit/cannon with vec2f

--- game_logic/src/unit.cpp
+++ game_logic/src/unit.cpp
@@ -9,29 +9,27 @@ namespace war_of_ages {
 unit_stats::unit_stats(float time_to_train_s_,
                        int initial_hp_,
                        float attack_cooldown_s_,
-                       int attack_radius_pxls_,
+                       float attack_radius_pxls_,
                        int damage_,
                        int cost_,
-                       int width_,
-                       int height_,
-                       int speed_)
+                       vec2f size_,
+                       float speed_)
     : time_to_train_s(time_to_train_s_),
       initial_hp(initial_hp_),
       attack_cooldown_s(attack_cooldown_s_),
       attack_radius_pxls(attack_radius_pxls_),
       damage(damage_),
       cost(cost_),
-      width_pxls(width_),
-      height_pxls(height_),
+      size(size_),
       speed(speed_) {
 }
 
 unit_stats unit::get_stats(unit_type type) noexcept {
     const static std::unordered_map<unit_type, unit_stats> stats{
-        {unit_type::PEASANT, unit_stats(2, 50, 0.5, 0, 25, 25, 100, 200, 5)},
-        {unit_type::ARCHER, unit_stats(3, 70, 1, 100, 30, 50, 100, 200, 5)},
-        {unit_type::CHARIOT, unit_stats(4, 100, 1.5, 0, 40, 100, 200, 160, 10)},
-        {unit_type::STONE_TOWER, unit_stats(0, 5000, 0, 0, 0, 0, 0, 0, 0)}};
+        {unit_type::PEASANT, unit_stats(2, 50, 0.5, 0, 25, 25, {100, 200}, 5)},
+        {unit_type::ARCHER, unit_stats(3, 70, 1, 100, 30, 50, {100, 200}, 5)},
+        {unit_type::CHARIOT, unit_stats(4, 100, 1.5, 0, 40, 100, {200, 160}, 10)},
+        {unit_type::STONE_TOWER, unit_stats(0, 5000, 0, 0, 0, 0, {0, 0}, 0)}};
     return stats.at(type);
 }
 
@@ -60,11 +58,11 @@ void unit::update(unit &enemy, const std::optional<unit> &next_allied_unit, floa
     } else if (!next_allied_unit) {
         move(dt, FIELD_LENGTH_PXLS - enemy.position() + 1);
     } else {
-        move(dt, next_allied_unit->position() - get_stats(next_allied_unit->type()).width_pxls + 1);
+        move(dt, next_allied_unit->position() - get_stats(next_allied_unit->type()).size.x + 1);
     }
 }
 
-int unit::position() const noexcept {
+float unit::position() const noexcept {
     return m_position;
 }
 
@@ -72,12 +70,12 @@ int unit::remaining_hp() const noexcept {
     return m_remaining_hp;
 }
 
-int unit::is_in(int x, int y) const noexcept {
-    return m_position - get_stats(m_type).width_pxls < x && x <= m_position && 0 <= y &&
-           y < get_stats(m_type).height_pxls;
+int unit::is_in(vec2f point) const noexcept {
+    return m_position - get_stats(m_type).size.x < point.x && point.x <= m_position && 0 <= point.y &&
+           point.y < get_stats(m_type).size.y;
 }
 
-int unit::dist(unit &enemy) const noexcept {
+float unit::dist(unit &enemy) const noexcept {
     return std::max(0.f, (FIELD_LENGTH_PXLS - enemy.position() - 1) - position());
 }
 
@@ -85,12 +83,12 @@ unit_type unit::type() const noexcept {
     return m_type;
 }
 
-void unit::move(float dt, int bound_position) {
-    int dist = bound_position - m_position;
+void unit::move(float dt, float bound_position) {
+    float dist = bound_position - m_position;
     if (dist <= 0) {
         return;
     }
-    m_position += static_cast<int>(std::min(dt * get_stats(m_type).speed, 1.f * dist));
+    m_position += std::min(dt * get_stats(m_type).speed, dist);
 }
 
 }  // namespace war_of_ages
