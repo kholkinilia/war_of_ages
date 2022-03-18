f6e666e2459f92c78f3d2b68ffe79789d58357ce
Ilya <iluxa.kholkin@gmail.com>
2022-03-18 17:47:14 +0300

    Remove useless comment

--- game_logic/include/game_state.h
+++ game_logic/include/game_state.h
@@ -21,7 +21,7 @@ public:
                 const std::vector<std::unique_ptr<game_command>> &p2_commands,
                 float dt);
     [[nodiscard]] std::pair<player_snapshot, player_snapshot> snapshot_players() const;
-    [[nodiscard]] game_status get_game_status() const;  // -1 if p1 won, 0 if game is processing, 1 if p2 won
+    [[nodiscard]] game_status get_game_status() const;
 };
 
 }  // namespace war_of_ages
