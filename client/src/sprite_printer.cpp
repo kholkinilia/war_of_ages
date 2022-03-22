ed7189428744177333b315c7cc2a2cc72346858b
Timur <Kudashev>
2022-03-22 10:05:48 +0300

    Fixed representation and shooting problem

--- client/src/sprite_printer.cpp
+++ client/src/sprite_printer.cpp
@@ -104,8 +104,6 @@ void sprite_printer::print_units(sf::RenderWindow *window,
             unit_hp_in.setFillColor({255, 0, 0, 255});
 
         float x_pos = unit.position() + TOWER_WIDTH - DELTA, y_pos, hp_len;
-        if (unit.type() != unit_type::STONE_TOWER)
-            x_pos -= 2 * DELTA;
         if (side == sprite_supplier::player_side::RIGHT) {
             x_pos = ROAD_WIDTH - x_pos;
         }
@@ -152,8 +150,8 @@ void sprite_printer::print_bullets(sf::RenderWindow *window,
     for (auto bullet : bullets) {
         bullet_picture = sprite_supplier::get_instance().get_bullet_sprite(bullet.type(), side);
 
-        float x_pos = bullet.pos().x + TOWER_WIDTH - 3 * DELTA,
-              y_pos = BACKGROUND_HEIGHT - DELTA - bullet.pos().y - bullet.stats().size.y;
+        float x_pos = bullet.pos().x + TOWER_WIDTH - DELTA,
+              y_pos = BACKGROUND_HEIGHT - DELTA - bullet.pos().y;
         if (side == sprite_supplier::player_side::RIGHT) {
             x_pos = ROAD_WIDTH - x_pos;
         }
@@ -173,7 +171,7 @@ void sprite_printer::print_cannons(sf::RenderWindow *window,
         cannon_slot_picture =
             sprite_supplier::get_instance().get_cannon_slot_sprite({age_type::STONE, i + 1}, side);
 
-        float x_pos = TOWER_WIDTH - 2.5 * DELTA, y_pos = CANNONS_SLOTS_COORD_Y[i];
+        float x_pos = TOWER_WIDTH - DELTA + CANNONS_SLOTS_COORD_X[i], y_pos = CANNONS_SLOTS_COORD_Y[i] + DELTA;
         if (side == sprite_supplier::player_side::RIGHT) {
             x_pos = ROAD_WIDTH - x_pos;
         }
