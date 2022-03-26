86b2a8bae3c08b173c30f93ef608d91b52fe9ac6
Ilya <iluxa.kholkin@gmail.com>
2022-03-26 03:19:55 +0300

    Return bot back

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,12 +1,12 @@
 #include <TGUI/Backend/SFML-Graphics.hpp>
 #include <chrono>
 #include <memory>
+#include <random>
 #include "../include/client.h"
-#include "../include/game_object_size_constants.h"
 #include "../include/end_game_screen.h"
+#include "../include/game_object_size_constants.h"
 #include "../include/screens.h"
 #include "../include/ui_functions.h"
-#include <random>
 
 namespace war_of_ages {
 
@@ -34,7 +34,7 @@ int main() {
     war_of_ages::setup_fps(gui);
 
     float last_added_time = -100;
-    float next_creation = 5;
+    float next_creation = rand() % 4 + 1;
 
     while (window.isOpen()) {
         war_of_ages::handle_window_events(gui, &window, &view);
@@ -51,6 +51,16 @@ int main() {
         war_of_ages::update_screens(gui, war_of_ages::current_state, &window);
 
         gui.draw();
+
+        float cur_time = 1.f * clock() / CLOCKS_PER_SEC;
+        if (war_of_ages::current_state.get_cur_screen() == war_of_ages::screen::GAME_SCREEN &&
+            cur_time - last_added_time >= next_creation) {
+            last_added_time = cur_time;
+            next_creation = rand() % 4 + 1;
+            std::vector<std::unique_ptr<war_of_ages::game_command>> v;
+            v.push_back(std::make_unique<war_of_ages::buy_unit_command>(rand() % 3));
+            war_of_ages::current_state.get_cur_game_state()->update({}, v, cur_time);
+        }
 
         window.display();
         window.setView(view);
