474170ba9f321731f6f58fe82947f43109d54c63
Ilya <iluxa.kholkin@gmail.com>
2022-05-03 03:11:23 +0300

    Initial commit

---
+++ network/include/thread_safe_deque.h
@@ -0,0 +1,87 @@
+#ifndef WAR_OF_AGES_THREAD_SAFE_DEQUE_H
+#define WAR_OF_AGES_THREAD_SAFE_DEQUE_H
+
+#include <deque>
+#include <mutex>
+
+namespace war_of_ages {
+
+template <typename T>
+struct ts_deque {
+private:
+    std::deque<T> m_deque;
+    std::mutex m_mutex;
+
+public:
+    ts_deque() = default;
+
+    ts_deque &operator=(const ts_deque &) = delete;
+    ts_deque &operator=(ts_deque &&) = default;
+    ts_deque(const ts_deque &) = delete;
+    ts_deque(ts_deque &&) = default;
+
+    ~ts_deque() = default;
+
+    [[nodiscard]] std::size_t size() const {
+        std::unique_lock lock(m_mutex);
+        return m_deque.size();
+    }
+
+    [[nodiscard]] bool empty() const {
+        std::unique_lock lock(m_mutex);
+        return m_deque.empty();
+    }
+
+    [[nodiscard]] const T &front() const {
+        std::unique_lock lock(m_mutex);
+        return m_deque.front();
+    }
+
+    [[nodiscard]] const T &back() const {
+        std::unique_lock lock(m_mutex);
+        return m_deque.back();
+    }
+
+    void push_front(const T &val) & {
+        std::unique_lock lock(m_mutex);
+        m_deque.push_front(val);
+    }
+
+    void push_front(T &&val) & {
+        std::unique_lock lock(m_mutex);
+        m_deque.push_back(std::move(val));
+    }
+
+    void push_back(const T &val) & {
+        std::unique_lock lock(m_mutex);
+        m_deque.push_back(val);
+    }
+
+    void push_back(T &&val) & {
+        std::unique_lock lock(m_mutex);
+        m_deque.push_back(std::move(val));
+    }
+
+    T pop_front() & {
+        std::unique_lock lock(m_mutex);
+        auto res = m_deque.front();
+        m_deque.pop_front();
+        return res;
+    }
+
+    T pop_back() & {
+        std::unique_lock lock(m_mutex);
+        auto res = m_deque.back();
+        m_deque.pop_back();
+        return res;
+    }
+
+    void clear() {
+        std::unique_lock lock(m_mutex);
+        m_deque.clear();
+    }
+};
+
+}  // namespace war_of_ages
+
+#endif  // WAR_OF_AGES_THREAD_SAFE_DEQUE_H
