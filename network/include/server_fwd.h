501791714015f5f78a8dbe9ec3c583f0af839fa4
Vakhtang <evm35rus@gmail.com>
2022-05-05 01:48:26 +0300

    wrapped connetction & server fwds in namespace war_of_ages

--- network/include/server_fwd.h
+++ network/include/server_fwd.h
@@ -1,7 +1,9 @@
 #ifndef WAR_OF_AGES_SERVER_FWD_H
 #define WAR_OF_AGES_SERVER_FWD_H
 
+namespace war_of_ages {
 template <typename T>
 struct server_inteface;
+}
 
 #endif  // WAR_OF_AGES_SERVER_FWD_H
