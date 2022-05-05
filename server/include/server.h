cf22207484354650d17f5d09d36276e79bce3eb1
Vakhtang <evm35rus@gmail.com>
2022-05-06 01:20:07 +0300

    define server structure

---
+++ server/include/server.h
@@ -0,0 +1,42 @@
+#ifndef WAR_OF_AGES_NET_SERVER_H
+#define WAR_OF_AGES_NET_SERVER_H
+
+#include <cstdint>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include "../../common/include/messages_type.h"
+#include "../../common/network/include/server_interface.h"
+#include "../include/room_matchmaker.h"
+
+namespace war_of_ages {
+
+enum class user_status {
+    AUTHORIZATION,
+    MENU,
+    RANDOMGAME,
+    ROOM,
+    TOURNAMENT,
+};
+
+struct server final : server_interface<messages_type> {
+public:
+    explicit server(std::uint16_t port);
+    void send_message(const std::string &handle, const message<messages_type> &msg);
+
+private:
+    std::unordered_map<std::uint32_t, std::shared_ptr<connection<messages_type>>> m_connection_by_id;
+    std::unordered_map<std::uint32_t, std::string> m_handle_by_id;
+    std::unordered_map<std::string, std::uint32_t> m_id_by_handle;
+    std::unordered_map<std::uint32_t, user_status> m_status_by_id;
+    std::mutex m_mutex_status;
+
+protected:
+    bool on_client_connect(std::shared_ptr<connection<messages_type>> client) override;
+    void on_client_disconnect(std::shared_ptr<connection<messages_type>> client) override;
+    void on_message(std::shared_ptr<connection<messages_type>> client,
+                    const message<messages_type> &msg) override;
+};
+}  // namespace war_of_ages
+
+#endif  // WAR_OF_AGES_NET_SERVER_H
---
+++ server/src/server.cpp
@@ -0,0 +1,40 @@
+#include "../include/server.h"
+#include <cassert>
+
+namespace war_of_ages {
+
+server::server(std::uint16_t port): server_interface<messages_type>(port) {}
+
+void server::send_message(const std::string &handle, const message<messages_type> &msg) {
+    auto it_id = m_id_by_handle.find(handle);
+    assert(it_id != m_id_by_handle.end());
+    auto it_connection = m_connection_by_id.find(it_id->second);
+    assert(it_connection != m_connection_by_id.end());
+    server_interface::send_message(it_connection->second, msg);
+}
+
+bool server::on_client_connect(std::shared_ptr<connection<messages_type>> client) {
+    m_connection_by_id.insert({client->get_id(), client});
+    std::unique_lock l(m_mutex_status);
+    m_status_by_id.insert({client->get_id(), user_status::MENU});
+    return true;
+}
+
+void server::on_client_disconnect(std::shared_ptr<connection<messages_type>> client) {
+    std::uint32_t id = client->get_id();
+    m_connection_by_id.erase(id);
+    auto handle_it = m_handle_by_id.find(id);
+    if (handle_it != m_handle_by_id.end()) {
+        m_id_by_handle.erase(handle_it->second);
+        m_handle_by_id.erase(handle_it);
+    }
+    std::unique_lock l(m_mutex_status);
+    m_status_by_id.erase(id);
+    // TODO: tournament & matches logic
+}
+
+void server::on_message(std::shared_ptr<connection<messages_type>> client, const message<messages_type> &msg) {
+    // TODO: implement
+}
+
+}  // namespace war_of_ages
