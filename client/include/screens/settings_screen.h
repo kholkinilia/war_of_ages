d45172a4639511cc8311146683194a549cc1054a
Vakhtang <evm35rus@gmail.com>
2022-05-07 20:10:10 +0300

    fixed local path (stupid CLion)

--- client/include/screens/settings_screen.h
+++ client/include/screens/settings_screen.h
@@ -1,7 +1,7 @@
 #ifndef WAR_OF_AGES_SETTINGS_SCREEN_H
 #define WAR_OF_AGES_SETTINGS_SCREEN_H
 
-#include "../../../../../Documents/TGUI/include/TGUI/Backend/SFML-Graphics.hpp"  // tgui::Gui
+#include <TGUI/Backend/SFML-Graphics.hpp>  // tgui::Gui
 
 namespace war_of_ages {
 void settings_screen_init(sf::View &v, tgui::Gui &gui);
