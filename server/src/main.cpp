328a8ae1c46afecfa28091214085b7345ea9e72d
Vakhtang <evm35rus@gmail.com>
2022-05-09 06:53:00 +0300

    some thoughts about limiting frequency of updating games inside server

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -10,6 +10,7 @@ int main() {
             war_of_ages::server::server::instance().update(-1, true);
         }
     }).detach();
+    ///  Looks normal for both clients
     while (true) {
         int n = 3000000;
         for (long long i = 0; i != n + 1; ++i) {
@@ -19,4 +20,27 @@ int main() {
             }
         }
     }
+    ///    For other client (not host) looks like very small fps
+    ///    +- normal image if replace 10 with number <= 3 (wtf ???)
+    //    include <chrono>
+    //    ...
+    //    std::chrono::time_point last_sending = std::chrono::system_clock::now();
+    //    while (true) {
+    //        std::chrono::time_point current_sending = std::chrono::system_clock::now();
+    //        using ms = std::chrono::duration<float, std::milli>;
+    //        if (std::chrono::duration_cast<ms>(current_sending - last_sending).count() >= 10) {
+    //            war_of_ages::server::game_handler::instance().update_games();
+    //            last_sending = current_sending;
+    //        }
+    //    }
+
+    ///  Crashes with uncaught exception ( .at() )
+//    #include <chrono>
+//    ...
+//    while (true) {
+//        std::chrono::time_point start = std::chrono::system_clock::now();
+//        war_of_ages::server::game_handler::instance().update_games();
+//        std::chrono::time_point end = std::chrono::system_clock::now();
+//        std::this_thread::sleep_for(std::chrono::milliseconds(17 - (end - start).count()));
+//    }
 }
