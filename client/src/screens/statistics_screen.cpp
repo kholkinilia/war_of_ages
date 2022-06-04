7491dc778bc61d88097888ff663c80592ea6ed97
Timur <Kudashev>
2022-06-05 00:22:32 +0300

    Fixed statistics_screen return button

--- client/src/screens/statistics_screen.cpp
+++ client/src/screens/statistics_screen.cpp
@@ -65,7 +65,7 @@ void screen_handler::statistics_screen_init() {
     statistics_screen_group->add(panel, "stats_panel");
 
     tgui::Theme black_theme("../client/resources/tgui_themes/Black.txt");
-    auto return_button = tgui::Button::create("В главное меню");
+    auto return_button = tgui::Button::create("Назад");
     return_button->setRenderer(black_theme.getRenderer("Button"));
     return_button->setTextSize(30);
     return_button->onPress(
@@ -73,7 +73,7 @@ void screen_handler::statistics_screen_init() {
     return_button->setPosition("30%", "86%");
     return_button->setSize("40%", "10%");
     statistics_screen_group->add(return_button, "return_button");
-    m_gui.add(statistics_screen_group, screen_handler::screen_id.at(screen_handler::screen_type::STATISTICS));
+    m_gui.add(statistics_screen_group, screen_handler::screen_id.at(screen_handler::screen_type::PREVIOUS_MENU));
     m_gui.get(screen_handler::screen_id.at(screen_handler::screen_type::STATISTICS))->setVisible(false);
 }
 }  // namespace war_of_ages::client
